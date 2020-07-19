61317ac648f5435e115db3b84a0eb3a0450739b5
akbfitra <akbarfitralistyono@gmail.com>
2020-07-19 09:57:02 +0700

    :fire:

---
+++ bilanganPrima.js
@@ -0,0 +1,18 @@
+/* 
+==============
+Bilangan Prima
+==============
+​
+Bilangan prima merupakan bilangan yang hanya habis dibagi oleh angka 1 dan angka itu sendiri.
+Buat sebuah program untuk mengecek apakah sebuah angka merupakan angka prima atau bukan.
+ */
+​
+function prima(num) {
+
+}
+console.log(prima(1)) //false
+console.log(prima(2)) //true
+console.log(prima(5)) //true
+console.log(prima(45)) //false
+console.log(prima(23)) //true
+console.log(prima(230)) //false
